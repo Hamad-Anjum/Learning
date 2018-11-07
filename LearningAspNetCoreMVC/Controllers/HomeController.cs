942e7ed63aa9734648884ea1d2db38f33954b739
Hamad-Anjum <hamadanjum15@outlook.com>
2018-11-07 21:06:45 +0500

    First Commit

---
+++ LearningAspNetCoreMVC/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using LearningAspNetCoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningAspNetCoreMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Product myProduct = new Product
+            {
+                ProductID = 1,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Category = "Watersports",
+                Price = 275M
+            };
+
+            return View(myProduct);
+        }
+    }
+}
